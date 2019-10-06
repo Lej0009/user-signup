a7d2eb87d2c14e51c0daccbc7dc17dcd96104457
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-05 22:35:48 -0400

    added create template for main page

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import flask from flask
+from flask import Flask, render_template
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
 
-<!DOCTYPE HTML>
-<html>
-    <head>
 
-    </head>
+@app.route("/")
+def index():
+    return render_template('create.html')
 
-    <body>
-        <h1>Signup</h1>
-
-        <form action = "/" methods = "POST">
-            Username <input type="text" name="username" value="">
-            Password <input type="text" name="password" value="">
-            Verify Password <input type="text" name="verify_password" value="">
-            Email (optional) <input type="text" name="email" value="">
-            <input type="submit"></input>
-        </form>
-
-    </body>
-</html>
+app.run()
