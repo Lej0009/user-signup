0325759deee8357fd9c4f688dbcf0b7b9d0cd5a5
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-07 14:27:52 -0400

    add cgi.escape for all fields

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ username = request.form['username']
 password = request.form['password']
 verify_password = request.form['verify-pw']
 
-    # if the user typed nothing at all, redirect and tell them the error
+# if the user typed nothing at all, redirect and tell them the error
 if (not username) or (username.strip() == ""):
     error = "Please specify a username."
     return redirect("/?error=" + error)
@@ -27,6 +27,13 @@ if (not verify_password) or (verify_password.strip() == ""):
 if password != verify_password:
     error = "Passwords do not match."
     return redirect("/?error=" + error)
+
+
+# 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+username_escaped = cgi.escape(username, quote=True)
+password_escaped = cgi.escape(password, quote=True)
+verify_password_escaped = cgi.escape(verify_password, quote=True)
+email_escaped = cgi.escape(email, quote=True)
 
 
 
