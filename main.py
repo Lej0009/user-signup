55660608c7b609121772f6fbb15528cc138eabb7
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-07 14:24:36 -0400

    add if statements to handle no input and passwords not matching

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ app.config['DEBUG'] = True
 
 
 
+# look inside the request to figure out what the user typed
+username = request.form['username']
+password = request.form['password']
+verify_password = request.form['verify-pw']
+
+    # if the user typed nothing at all, redirect and tell them the error
+if (not username) or (username.strip() == ""):
+    error = "Please specify a username."
+    return redirect("/?error=" + error)
+
+if (not password) or (password.strip() == ""):
+    error = "Please specify a password."
+    return redirect("/?error=" + error)
+
+if (not verify_password) or (verify_password.strip() == ""):
+    error = "Please verify password."
+    return redirect("/?error=" + error)
+
+if password != verify_password:
+    error = "Passwords do not match."
+    return redirect("/?error=" + error)
+
+
+
 @app.route("/")
 def index():
     return render_template('base.html')
