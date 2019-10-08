ceed5edbbe082d4b0909ffd037bf228edcde09c1
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-08 14:14:58 -0400

    change html code to starter

--- main.py
+++ main.py
@@ -10,24 +10,30 @@ def verify():
     # look inside the request to figure out what the user typed
     username = request.form['username']
     password = request.form['password']
-    verify_password = request.form['verify-pw']
+    verify_password = request.form['verify-password']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+    email_error = ''
 
     # if the user typed nothing at all, redirect and tell them the error
     if (not username) or (username.strip() == ""):
-        error = "Please specify a username."
-        return redirect("/?error=" + error)
+        username_error = "Please specify a username."
+
 
     if (not password) or (password.strip() == ""):
-        error = "Please specify a password."
-        return redirect("/?error=" + error)
+        password_error = "Please specify a password."
+        # return redirect("/?error=" + error)
 
     if (not verify_password) or (verify_password.strip() == ""):
-        error = "Please verify password."
-        return redirect("/?error=" + error)
+        verify_password_error = "Please verify password."
+        # return redirect("/?error=" + error)
 
     if password != verify_password:
-        error = "Passwords do not match."
-        return redirect("/?error=" + error)
+        password_match_error = "Passwords do not match."
+        # return redirect("/?error=" + error)
 
 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
@@ -37,6 +43,10 @@ def verify():
     email_escaped = cgi.escape(email, quote=True)
 
 
+    if not username_error and not password_error and not verify_password_error and not email_error:
+        #success message
+    else:
+        return render_template('base.html', username_error, password_error, verify_password_error, password_match_error)
 
 @app.route("/")
 def index():
---
+++ mertyce/main.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+import jinja2
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+   return render_template('signup.html')
+
+@app.route("/signup", methods=['POST'])
+def user_hello():
+   user_name = request.form['user_name']
+   return render_template('welcome.html', user_name=user_name)
+
+def is_string():
+   try:
+       string == str
+       return True
+   except ValueError:
+       return False
+
+def pw_rules():
+   try:
+       if not ().isspace():
+           return True
+   except ValueError:
+       return False
+
+def verify_email(user_email):
+   try:
+       if '@' and '.com' in user_email:
+           return True
+   except ValueError:
+       return False
+
+@app.route("/signup", methods=['POST'])
+def user_valid():
+   user_name = request.form['user_name']
+   user_pw = request.form['user_pw']
+   verify_pw = request.form['verify_pw']
+   user_email = request.form['user_email']
+   username_error = ''
+   userpw_error = ''
+   verifypw_error = ''
+   useremail_error = ''
+
+   if not is_string(user_name):
+       username_error = 'Not a valid username. Must be between 3 and 20 characters.'
+       user_name = ''
+   else:
+       if user_name > 20 and user_name < 3:
+           username_error = 'Please select a username between 3 and 20 characters.'
+           user_name = ''
+
+   if not pw_rules(user_pw):
+       userpw_error = "Not a vaild password. Please enter atleast 8 characters."
+       user_pw = ''
+   else:
+       if len(user_pw) < 8:
+           userpw_error = 'Your password must be atleast 8 characters long.'
+           user_pw = ''
+
+   if user_pw != verify_pw:
+       verifypw_error = 'Your passwords must match.'
+       verify_pw = ''
+
+   if not verify_email(user_email):
+       useremail_error = ' Please enter your email.'
+       user_email = ''
+
+   if not username_error and not userpw_error and not useremail_error:
+       user_name = str(user_name)
+       return redirect('/welcome?username={0}'.format(user_name))
+   else:
+       return render_template('welcome.html', user_name=user_name)
+
+app.run()
