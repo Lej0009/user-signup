1309aefe9c0e2f5ee0d108e8fde56d563801c4d6
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-08 11:47:01 -0400

    change the address for verify from /a to /

--- main.py
+++ main.py
@@ -5,40 +5,42 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
+app.route("/", methods=['POST'])
+def verify():
+    # look inside the request to figure out what the user typed
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify-pw']
 
-# look inside the request to figure out what the user typed
-username = request.form['username']
-password = request.form['password']
-verify_password = request.form['verify-pw']
+    # if the user typed nothing at all, redirect and tell them the error
+    if (not username) or (username.strip() == ""):
+        error = "Please specify a username."
+        return redirect("/?error=" + error)
 
-# if the user typed nothing at all, redirect and tell them the error
-if (not username) or (username.strip() == ""):
-    error = "Please specify a username."
-    return redirect("/?error=" + error)
+    if (not password) or (password.strip() == ""):
+        error = "Please specify a password."
+        return redirect("/?error=" + error)
 
-if (not password) or (password.strip() == ""):
-    error = "Please specify a password."
-    return redirect("/?error=" + error)
+    if (not verify_password) or (verify_password.strip() == ""):
+        error = "Please verify password."
+        return redirect("/?error=" + error)
 
-if (not verify_password) or (verify_password.strip() == ""):
-    error = "Please verify password."
-    return redirect("/?error=" + error)
-
-if password != verify_password:
-    error = "Passwords do not match."
-    return redirect("/?error=" + error)
+    if password != verify_password:
+        error = "Passwords do not match."
+        return redirect("/?error=" + error)
 
 
-# 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-username_escaped = cgi.escape(username, quote=True)
-password_escaped = cgi.escape(password, quote=True)
-verify_password_escaped = cgi.escape(verify_password, quote=True)
-email_escaped = cgi.escape(email, quote=True)
+    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+    username_escaped = cgi.escape(username, quote=True)
+    password_escaped = cgi.escape(password, quote=True)
+    verify_password_escaped = cgi.escape(verify_password, quote=True)
+    email_escaped = cgi.escape(email, quote=True)
 
 
 
 @app.route("/")
 def index():
-    return render_template('base.html')
+    encoded_error = request.args.get("error")
+    return render_template('base.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
